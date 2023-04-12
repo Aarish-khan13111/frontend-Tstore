886e6ffae4253f9443ea02e4c90e8eb56a4fc981
aarish <aarishkhan13111@gmail.com>
2023-04-13 02:32:42 +0530

    working on manage product

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
-import { getAllCategory } from "./helper/adminapicall";
+import { createProduct, getAllCategory } from "./helper/adminapicall";
+import { isAutheticated } from "../auth/helper";
 
 const AddProduct = () => {
+  const { user, token } = isAutheticated();
+
   const [values, setValues] = useState({
     name: "",
     description: "",
@@ -50,28 +53,49 @@ const AddProduct = () => {
     preload();
   }, []);
 
-  const onSubmit = () => {
-    //
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: "", loading: true });
+    createProduct(user._id, token, formData)
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            description: "",
+            price: "",
+            photo: "",
+            stock: "",
+            loading: false,
+            createdProduct: data.name,
+          });
+        }
+      })
+      .catch();
   };
+
   const handleChange = (name) => (event) => {
-    //
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
   };
+
+  //success Message
+  const successMessage = () => (
+    <div
+      className="alert alert-success mt-3"
+      style={{ display: createdProduct ? "" : "none" }}
+    >
+      <h4>{createdProduct} created successfully</h4>
+    </div>
+  );
+
   //create product form
   const createProductForm = () => (
     <form>
-      <div className="form-group">
-        <label className="btn btn-block btn-info mt-3 border rounded">
-          <span className="text-white mr-3">Post photo</span>
-          <input
-            onChange={handleChange("photo")}
-            type="file"
-            name="photo"
-            accept="image"
-            placeholder="choose a file"
-          />
-        </label>
-      </div>
-      <div className="form-group">
+      <div className="form-group mt-3">
         <input
           onChange={handleChange("name")}
           name="photo"
@@ -104,21 +128,36 @@ const AddProduct = () => {
           className="form-control"
           placeholder="Category"
         >
-          <option>Select</option>
-          <option value="a">a</option>
-          <option value="b">b</option>
+          <option>Select Category</option>
+          {categories &&
+            categories.map((cate, index) => (
+              <option key={index} value={cate._id}>
+                {cate.name}
+              </option>
+            ))}
         </select>
       </div>
       <div className="form-group">
         <input
-          onChange={handleChange("quantity")}
+          onChange={handleChange("stock")}
           type="number"
           className="form-control"
           placeholder="Quantity"
           value={stock}
         />
       </div>
-
+      <div className="form-group">
+        <label className="btn btn-block btn-info mt-3 border rounded">
+          <span className="text-white mr-3">Post photo</span>
+          <input
+            onChange={handleChange("photo")}
+            type="file"
+            name="photo"
+            accept="image"
+            placeholder="choose a file"
+          />
+        </label>
+      </div>
       <button
         type="submit"
         onClick={onSubmit}
@@ -135,7 +174,10 @@ const AddProduct = () => {
       className="container bg-dark p-4"
     >
       <div className="row bg-light rounded">
-        <div className="col-md-8 offset-md-2">{createProductForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {successMessage()}
+          {createProductForm()}
+        </div>
       </div>
     </Base>
   );
